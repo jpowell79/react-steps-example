ca3ae4457772510dba2fafef33f8f30959b37fa6
jpowell79 <jpowell79@ymail.com>
2024-01-11 18:00:35 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -34,24 +34,24 @@ function App() {
           <p className="message">
             Step {step}: {messages[step - 1]}
           </p>
-
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button onClick={handlePrevious}>Previous</Button>
+            <Button onClick={handleNext}>Next</Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: "#7950f2", color: "#fff" }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
